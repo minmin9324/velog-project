a3c341f15d96b77cb3bde3d301fc3a46b9970b2b
minmin9324 <minmin93240@gmail.com>
2021-11-20 15:39:46 +0900

    [ADD]이전페이지,다음페이지 이동 기능

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,7 @@
-import React, {useState, useEffect} from "react";
-import axios from 'axios';
-import styled, {createGlobalStyle} from "styled-components";
-import PostCard from '../component/PostCard';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styled, { createGlobalStyle } from "styled-components";
+import PostCard from "../component/PostCard";
 import { initialState } from "./data";
 import DeleteModal from "../component/DeleteModal";
 
@@ -12,63 +12,63 @@ body {
 `;
 
 const MainWrapper = styled.div`
-margin-top: 100px;
-width: 1728px;
-margin-left: auto;
-margin-right: auto;
-@media (max-width: 1919px) {
-  width: 1376px;
-}
-@media (max-width: 1440px) {
-  width: 1024px;
-}
-@media (max-width: 1056px) {
-  width: calc(100% - 2rem);
-}
-
+  margin-top: 100px;
+  width: 1728px;
+  margin-left: auto;
+  margin-right: auto;
+  @media (max-width: 1919px) {
+    width: 1376px;
+  }
+  @media (max-width: 1440px) {
+    width: 1024px;
+  }
+  @media (max-width: 1056px) {
+    width: calc(100% - 2rem);
+  }
 `;
 
 const PostList = styled.div`
-display: flex;
-margin: -1rem;
-// grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-// margin-left: 100px;
-// margin-bottom: 100px;
-flex-wrap: wrap;
-`
+  display: flex;
+  margin: -1rem;
+  // grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  // margin-left: 100px;
+  // margin-bottom: 100px;
+  flex-wrap: wrap;
+`;
 
 const Main = () => {
   const [posts, setPosts] = useState([]);
   const [isModal, setIsModal] = useState(false);
   const [hasmorePost, setHasmorePost] = useState(true);
-  console.log("렌더링")
+  console.log("렌더링");
   console.log(posts);
   const deleteClick = (e) => {
     e.preventDefault();
     setIsModal(true);
-  }
+  };
 
   const listClick = () => {
     console.log(posts);
-  }
+  };
 
   const addPost = (data) => {
-    console.log('ADD POST')
+    console.log("ADD POST");
     console.log(data);
-    setPosts([...posts,...posts]);
-  }
-  useEffect(()=>{
-    axios.get('https://limitless-sierra-67996.herokuapp.com/v1/posts?limit=20')
-      .then(res => {
+    setPosts([...posts, ...posts]);
+  };
+  useEffect(() => {
+    axios
+      .get("https://limitless-sierra-67996.herokuapp.com/v1/posts?limit=20")
+      .then((res) => {
         console.log(res.data.results);
-        setPosts([...posts,...res.data.results])
-      })
-    
+        setPosts([...posts, ...res.data.results]);
+      });
+
     // console.log(posts);
-  },[]);
+  }, []);
 
   // useEffect(()=> {
-    
+
   //   function onScroll() {
   //     //scrollY: 얼마나 내렸는지, clientHeight: 화면 보이는 길이, scrollHeight: 총 길이
   //     console.log(window.scrollY, document.documentElement.clientHeight, document.documentElement.scrollHeight);
@@ -93,6 +93,7 @@ const Main = () => {
   //   };
 
   // }, [hasmorePost]);
+  console.log(posts);
   return (
     <>
       <Global />
@@ -100,11 +101,12 @@ const Main = () => {
         <button onClick={deleteClick}>삭제</button>
         <button onClick={listClick}>포스트리스트확인</button>
         <button onClick={addPost}>포스트추가</button>
-        {isModal && <DeleteModal isModal={setIsModal}/>}
+        {isModal && <DeleteModal isModal={setIsModal} />}
         <PostList>
-          {posts.map(post=>(<PostCard key={post.id} post={post}/>))}
+          {posts.map((post, index) => {
+            return <PostCard key={post.id} post={post} index={index} />;
+          })}
         </PostList>
-        
       </MainWrapper>
     </>
   );
